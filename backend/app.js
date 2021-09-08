f89448bfbffb7cfe1708e2ddd241a030b5450ff4
hungle88 <63668660+hungle88@users.noreply.github.com>
2021-09-08 15:43:17 -0500

    delete unnecessary codes

--- backend/app.js
+++ backend/app.js
@@ -65,55 +65,6 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage });
 
-// catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
-
-// app.get('/', (req, res) => {
-//   imgModel.find({}, (err, items) => {
-//       if (err) {
-//           console.log(err);
-//           res.status(500).send('An error occurred', err);
-//       }
-//       else {
-//           res.render('imagesPage', { items: items });
-//       }
-//   });
-// });
-
-// app.post('/', upload.single('image'), (req, res, next) => {
- 
-//   var obj = {
-//       name: req.body.name,
-//       desc: req.body.desc,
-//       img: {
-//           data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-//           contentType: 'image/png'
-//       }
-//   }
-//   imgModel.create(obj, (err, item) => {
-//       if (err) {
-//           console.log(err);
-//       }
-//       else {
-//           // item.save();
-//           res.redirect('/');
-//       }
-//   });
-// });
-
 
 app.listen(port, () => {
   console.log(`running on port ${port}`);
